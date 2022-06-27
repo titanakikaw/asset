b5244329b8e96b22294b2ce33d83e84a634eacde
Marvin Orsua HO-IT <marvin.orsua@everho.ph>
2022-06-27 18:05:12 +0800

    second

---
+++ client/newasset.php
@@ -0,0 +1,32 @@
+<?php
+require('../static_components/header.php');
+?>
+
+<div class="container-fluid" style="background-color: #f3f3f3;">
+    <div class="container" style="padding: 10px; ">
+        <div class="row">
+            <div class="col">
+                <div class="input-group input-group-sm mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Asset Code :</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="col">
+                <div class="input-group input-group-sm mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Asset Description :</span>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require('../static_components/footer.php');
+?>
