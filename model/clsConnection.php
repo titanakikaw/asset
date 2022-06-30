75ee80fcb0dc82824db183b6b5aa17a6f7fbe4bd
unknown <orsuam@gmail.com>
2022-06-30 16:04:13 +0800

    test

--- model/clsConnection.php
+++ model/clsConnection.php
@@ -7,9 +7,9 @@ class dbConnection
     // private $password = "donterase";
     // private $database = "EHELPDESK";
     private $servername = "(local)";
-    private $username = "marvin";
-    private $password = "K@t3Orsua166";
-    private $database = "monitoringsys";
+    private $username = "private";
+    private $password = "123";
+    private $database = "DRISM_ASSET_MONITORING ";
 
     function conn()
     {
